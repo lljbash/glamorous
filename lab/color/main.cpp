f8fe56743e72c4c0be5a7ee0f5c858f5f4ec5c42
Li Lingjie <llj_95@163.com>
2017-07-04 14:51:40 +0800

    fix color bugs

--- lab/color/main.cpp
+++ lab/color/main.cpp
@@ -6,7 +6,7 @@ using namespace glamorous;
 
 int main(int argc, char const *argv[]) {
     cv::Mat image = cv::imread("../a.jpg", 1);
-    ColorTransfer ct(new ColorDifferenceHSV());
+    ColorTransfer ct(new ColorDifferenceRGB());
     cv::Mat c = cv::Mat::zeros(1, 5, CV_8UC3);
     c.ptr<cv::Vec3b>()[0] = {216, 175, 157};
     c.ptr<cv::Vec3b>()[1] = {154, 170, 168};
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[]) {
     c.ptr<cv::Vec3b>()[3] = {96, 108, 99};
     c.ptr<cv::Vec3b>()[4] = {79, 76, 58};
     ct.initialize(image, c);
-    ct.apply(0.5);
+    ct.apply(0.4);
     cv::Mat dst = ct.get_dst();
     cv::imwrite("color.png", dst);
     printf("Ha, you like it, ha?\n");
