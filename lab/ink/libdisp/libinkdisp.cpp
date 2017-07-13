2fcbdcb7ee2de0631a9d974af11dd602ab7e3d63
Gao Tong <gaot14@mails.tsinghua.edu.cn>
2017-07-13 11:37:42 +0800

    Finish compiling .so file

---
+++ lab/ink/libdisp/libinkdisp.cpp
@@ -0,0 +1,123 @@
+//
+// MATLAB Compiler: 6.4 (R2017a)
+// Date: Thu Jul 13 11:34:25 2017
+// Arguments:
+// "-B""macro_default""-W""cpplib:libinkdisp""-T""link:lib""inkdisp.m""-d""libdi
+// sp"
+//
+
+#include <stdio.h>
+#define EXPORTING_libinkdisp 1
+#include "libinkdisp.h"
+
+static HMCRINSTANCE _mcr_inst = NULL;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static int mclDefaultPrintHandler(const char *s)
+{
+  return mclWrite(1 /* stdout */, s, sizeof(char)*strlen(s));
+}
+
+#ifdef __cplusplus
+} /* End extern "C" block */
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static int mclDefaultErrorHandler(const char *s)
+{
+  int written = 0;
+  size_t len = 0;
+  len = strlen(s);
+  written = mclWrite(2 /* stderr */, s, sizeof(char)*len);
+  if (len > 0 && s[ len-1 ] != '\n')
+    written += mclWrite(2 /* stderr */, "\n", sizeof(char));
+  return written;
+}
+
+#ifdef __cplusplus
+} /* End extern "C" block */
+#endif
+
+/* This symbol is defined in shared libraries. Define it here
+ * (to nothing) in case this isn't a shared library. 
+ */
+#ifndef LIB_libinkdisp_C_API
+#define LIB_libinkdisp_C_API /* No special import/export declaration */
+#endif
+
+LIB_libinkdisp_C_API 
+bool MW_CALL_CONV libinkdispInitializeWithHandlers(
+    mclOutputHandlerFcn error_handler,
+    mclOutputHandlerFcn print_handler)
+{
+    int bResult = 0;
+  if (_mcr_inst != NULL)
+    return true;
+  if (!mclmcrInitialize())
+    return false;
+    {
+        mclCtfStream ctfStream = 
+            mclGetEmbeddedCtfStream((void *)(libinkdispInitializeWithHandlers));
+        if (ctfStream) {
+            bResult = mclInitializeComponentInstanceEmbedded(   &_mcr_inst,
+                                                                error_handler, 
+                                                                print_handler,
+                                                                ctfStream);
+            mclDestroyStream(ctfStream);
+        } else {
+            bResult = 0;
+        }
+    }  
+    if (!bResult)
+    return false;
+  return true;
+}
+
+LIB_libinkdisp_C_API 
+bool MW_CALL_CONV libinkdispInitialize(void)
+{
+  return libinkdispInitializeWithHandlers(mclDefaultErrorHandler, mclDefaultPrintHandler);
+}
+
+LIB_libinkdisp_C_API 
+void MW_CALL_CONV libinkdispTerminate(void)
+{
+  if (_mcr_inst != NULL)
+    mclTerminateInstance(&_mcr_inst);
+}
+
+LIB_libinkdisp_C_API 
+void MW_CALL_CONV libinkdispPrintStackTrace(void) 
+{
+  char** stackTrace;
+  int stackDepth = mclGetStackTrace(&stackTrace);
+  int i;
+  for(i=0; i<stackDepth; i++)
+  {
+    mclWrite(2 /* stderr */, stackTrace[i], sizeof(char)*strlen(stackTrace[i]));
+    mclWrite(2 /* stderr */, "\n", sizeof(char)*strlen("\n"));
+  }
+  mclFreeStackTrace(&stackTrace, stackDepth);
+}
+
+
+LIB_libinkdisp_C_API 
+bool MW_CALL_CONV mlxInkdisp(int nlhs, mxArray *plhs[], int nrhs, mxArray *prhs[])
+{
+  return mclFeval(_mcr_inst, "inkdisp", nlhs, plhs, nrhs, prhs);
+}
+
+LIB_libinkdisp_CPP_API 
+void MW_CALL_CONV inkdisp(int nargout, mwArray& img2, const mwArray& img, const mwArray& 
+                          lambda, const mwArray& alpha, const mwArray& canny)
+{
+  mclcppMlfFeval(_mcr_inst, "inkdisp", nargout, 1, 4, &img2, &img, &lambda, &alpha, &canny);
+}
+
---
+++ lab/ink/libdisp/libinkdisp.h
@@ -0,0 +1,113 @@
+//
+// MATLAB Compiler: 6.4 (R2017a)
+// Date: Thu Jul 13 11:34:25 2017
+// Arguments:
+// "-B""macro_default""-W""cpplib:libinkdisp""-T""link:lib""inkdisp.m""-d""libdi
+// sp"
+//
+
+#ifndef __libinkdisp_h
+#define __libinkdisp_h 1
+
+#if defined(__cplusplus) && !defined(mclmcrrt_h) && defined(__linux__)
+#  pragma implementation "mclmcrrt.h"
+#endif
+#include "mclmcrrt.h"
+#include "mclcppclass.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(__SUNPRO_CC)
+/* Solaris shared libraries use __global, rather than mapfiles
+ * to define the API exported from a shared library. __global is
+ * only necessary when building the library -- files including
+ * this header file to use the library do not need the __global
+ * declaration; hence the EXPORTING_<library> logic.
+ */
+
+#ifdef EXPORTING_libinkdisp
+#define PUBLIC_libinkdisp_C_API __global
+#else
+#define PUBLIC_libinkdisp_C_API /* No import statement needed. */
+#endif
+
+#define LIB_libinkdisp_C_API PUBLIC_libinkdisp_C_API
+
+#elif defined(_HPUX_SOURCE)
+
+#ifdef EXPORTING_libinkdisp
+#define PUBLIC_libinkdisp_C_API __declspec(dllexport)
+#else
+#define PUBLIC_libinkdisp_C_API __declspec(dllimport)
+#endif
+
+#define LIB_libinkdisp_C_API PUBLIC_libinkdisp_C_API
+
+
+#else
+
+#define LIB_libinkdisp_C_API
+
+#endif
+
+/* This symbol is defined in shared libraries. Define it here
+ * (to nothing) in case this isn't a shared library. 
+ */
+#ifndef LIB_libinkdisp_C_API 
+#define LIB_libinkdisp_C_API /* No special import/export declaration */
+#endif
+
+extern LIB_libinkdisp_C_API 
+bool MW_CALL_CONV libinkdispInitializeWithHandlers(
+       mclOutputHandlerFcn error_handler, 
+       mclOutputHandlerFcn print_handler);
+
+extern LIB_libinkdisp_C_API 
+bool MW_CALL_CONV libinkdispInitialize(void);
+
+extern LIB_libinkdisp_C_API 
+void MW_CALL_CONV libinkdispTerminate(void);
+
+
+
+extern LIB_libinkdisp_C_API 
+void MW_CALL_CONV libinkdispPrintStackTrace(void);
+
+extern LIB_libinkdisp_C_API 
+bool MW_CALL_CONV mlxInkdisp(int nlhs, mxArray *plhs[], int nrhs, mxArray *prhs[]);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+
+/* On Windows, use __declspec to control the exported API */
+#if defined(_MSC_VER) || defined(__BORLANDC__)
+
+#ifdef EXPORTING_libinkdisp
+#define PUBLIC_libinkdisp_CPP_API __declspec(dllexport)
+#else
+#define PUBLIC_libinkdisp_CPP_API __declspec(dllimport)
+#endif
+
+#define LIB_libinkdisp_CPP_API PUBLIC_libinkdisp_CPP_API
+
+#else
+
+#if !defined(LIB_libinkdisp_CPP_API)
+#if defined(LIB_libinkdisp_C_API)
+#define LIB_libinkdisp_CPP_API LIB_libinkdisp_C_API
+#else
+#define LIB_libinkdisp_CPP_API /* empty! */ 
+#endif
+#endif
+
+#endif
+
+extern LIB_libinkdisp_CPP_API void MW_CALL_CONV inkdisp(int nargout, mwArray& img2, const mwArray& img, const mwArray& lambda, const mwArray& alpha, const mwArray& canny);
+
+#endif
+#endif
