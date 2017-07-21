2179ea40478fbf79b7ea02aa2549f45573048fc7
ywn14 <312369144@qq.com>
2017-07-21 10:51:54 +0800

    format main2.cpp

--- lab/ywn/generateGif/main2.cpp
+++ lab/ywn/generateGif/main2.cpp
@@ -5,7 +5,6 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
-#include <queue>
 
 using namespace std;
 using namespace cv;
@@ -19,24 +18,11 @@ public:
 };
 
 
+// global var
 bool** isVisit;
-bool** bfs_flag;
 int cnt;
 Mat _m;
 
-int randomDraw(vector<mPoint>& pVec, const int& N, Mat& resMat)
-{
-	int NUM = min(N, int(pVec.size()));
-	// cout << NUM << endl;
-	for (int i = 0; i < NUM; ++i)
-	{
-		int randIndex = rand() % pVec.size();
-		resMat.at<uchar>(pVec[randIndex].x, pVec[randIndex].y) = pVec[randIndex].gray;
-		pVec.erase(pVec.begin() + randIndex);
-	}
-	
-	return NUM;
-}
 
 bool cmp(const mPoint& a, const mPoint& b)
 {
@@ -45,52 +31,29 @@ bool cmp(const mPoint& a, const mPoint& b)
 
 mPoint go(int x, int y, Mat& resMat, const int& NUM, int deep)
 {
-	if (isVisit[x][y] || deep > 5000) 
+	if (isVisit[x][y])
 		return mPoint(x,y,deep);
-	
-	// cout << "GO: " << x << ", " << y << endl;
+
 	isVisit[x][y] = true;
 	resMat.at<uchar>(x, y) = _m.at<uchar>(x, y);
 	++cnt;
-	// if ((++cnt) % NUM == 0)
-	// {
-	// 	stringstream ss;
-	// 	ss << (cnt / NUM);
-	// 	string fooStr = ss.str() + ".jpg";
-	// 	imwrite(fooStr, resMat);
-	// }
 
 	// recursion
 	vector<mPoint> fooVec;
 	if (!isVisit[min(x + 1, _m.rows - 1)][y])
-	{
 		fooVec.push_back(mPoint(min(x + 1, _m.rows - 1), y, _m.at<uchar>(min(x + 1, _m.rows - 1), y)));
-	}
 	if (!isVisit[x][ min(y + 1, _m.cols - 1)])
-	{
 		fooVec.push_back(mPoint(x, min(y + 1, _m.cols - 1), _m.at<uchar>(x, min(y + 1, _m.cols - 1))));
-	}
-
 	if (!isVisit[max(x - 1, 0)][y])
-	{
 		fooVec.push_back(mPoint(max(x - 1, 0), y, _m.at<uchar>(max(x - 1, 0), y)));
-	}
 	if (!isVisit[x][ max(y - 1, 0)])
-	{
 		fooVec.push_back(mPoint(x, max(y - 1, 0), _m.at<uchar>(x, max(y - 1, 0))));	
-	}
-	
-	
 	
 	
 	sort(fooVec.begin(), fooVec.end(), cmp);
-	// for (int i = 0; i < fooVec.size(); ++i)
-	// 	cout << int(fooVec[i].gray) << ", ";
-	// cout << endl;
+
 	if (fooVec.size()==0)
-	{
 		return mPoint(x,y,deep);
-	}
 	for (int i = 0; i < 1; ++i)
 		return go(fooVec[i].x, fooVec[i].y, resMat, NUM, deep + 1);
 
@@ -103,10 +66,10 @@ void generateVideo(const int& FRAME_NUM)
     VideoWriter writer;  
       
     int codec = 0;  
-    int frameRate = 25;  
+    int frameRate = 20;  
     Size frameSize;  
 
-    for (int i = 1; i < FRAME_NUM; ++i)  
+    for (int i = 1; i <= FRAME_NUM; ++i)  
     {         
         stringstream ss;
         ss << i;
@@ -129,67 +92,8 @@ void generateVideo(const int& FRAME_NUM)
  
 }
 
-mPoint bfs_next(mPoint& mp){
 
-	for (int i = 0; i < _m.rows; ++i)
-		for (int j = 0; j < _m.cols; ++j)
-			bfs_flag[i][j] = 0;
-
-	queue<mPoint> q;
-	q.push(mp);
-	int cyc_cnt = 0;
-	int size = q.size();
-	while(!q.empty()){
-		cyc_cnt++;
-		if(cyc_cnt>10000) {
-			cout<<cyc_cnt<<" "<<q.size()<<endl;
-			break;
-		}
-
-		if (q.size()-size > 1)
-		{
-			cout<<cyc_cnt<<" "<<q.size()<<endl;	
-
-		}
-		size = q.size();
-		mPoint p = q.front();
-		q.pop();
-		bfs_flag[p.x][p.y] = 1;
-
-		// find neighbor if possible
-		int x = p.x;
-		int y = p.y;
-		if (!isVisit[min(x + 1, _m.rows - 1)][y])
-		{
-			return mPoint(min(x + 1, _m.rows - 1), y, _m.at<uchar>(min(x + 1, _m.rows - 1), y));
-		}
-		if (!isVisit[x][ min(y + 1, _m.cols - 1)])
-		{
-			return mPoint(x, min(y + 1, _m.cols - 1), _m.at<uchar>(x, min(y + 1, _m.cols - 1)));
-		}
-
-		if (!isVisit[max(x - 1, 0)][y])
-		{
-			return mPoint(max(x - 1, 0), y, _m.at<uchar>(max(x - 1, 0), y));
-		}
-		if (!isVisit[x][ max(y - 1, 0)])
-		{
-			return mPoint(x, max(y - 1, 0), _m.at<uchar>(x, max(y - 1, 0)));	
-		}
-		if(!bfs_flag[min(x+1,_m.rows-1)][y])
-			q.push(mPoint(min(x + 1, _m.rows - 1), y, _m.at<uchar>(min(x + 1, _m.rows - 1), y)));
-		if(!bfs_flag[x][ min(y + 1, _m.cols - 1)])
-			q.push(mPoint(x, min(y + 1, _m.cols - 1), _m.at<uchar>(x, min(y + 1, _m.cols - 1))));
-		if(!bfs_flag[max(x - 1, 0)][ y])
-			q.push(mPoint(max(x - 1, 0), y, _m.at<uchar>(max(x - 1, 0), y)));
-		if(!bfs_flag[x] [max(y - 1, 0)])
-			q.push(mPoint(x, max(y - 1, 0), _m.at<uchar>(x, max(y - 1, 0))));	
-	}
-
-	return mPoint(-1,-1,0);
-}
-
-mPoint ch_next(mPoint& mp){
+mPoint getNextPoint(mPoint& mp){
 	int dist = 0;
 	int x = mp.x;
 	int y = mp.y;
@@ -221,11 +125,11 @@ mPoint ch_next(mPoint& mp){
 	return mPoint(-1, -1, -1);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 	srand((unsigned int) time(0));
 
-	string srcPath = "a.jpg";
+	string srcPath(argv[1]);
 	_m = imread(srcPath, CV_LOAD_IMAGE_GRAYSCALE);
 	vector<mPoint> pVec;
 	for (int i = 0; i < _m.rows; ++i)
@@ -237,30 +141,9 @@ int main()
 
 	const int FRAME_NUM = 400;
 	const int NUM = pVec.size() / FRAME_NUM;	
-/*	Mat resMat = 255 * Mat::ones(_m.rows, _m.cols, CV_8UC1);
-	cout << "resMat = " << resMat.cols << " * " <<  resMat.rows << endl;
-	int cnt = 0;
-	while (1)
-	{
-		cout << cnt << endl;
-		int ret = randomDraw(pVec, NUM, resMat);
-		if (ret == 0)
-			break;
-		stringstream ss;
-		ss << (cnt++);
-		string fooStr = ss.str() + ".jpg";
-		imwrite(fooStr, resMat);
-		// waitKey(0);
-	}
-*/
 
-	bfs_flag = new bool*[_m.rows];
-	for (int i = 0; i < _m.rows; ++i)
-		bfs_flag[i] = new bool[_m.cols];
-	for (int i = 0; i < _m.rows; ++i)
-		for (int j = 0; j < _m.cols; ++j)
-			bfs_flag[i][j] = 0;
 
+	// init 
 	isVisit = new bool*[_m.rows];
 	for (int i = 0; i < _m.rows; ++i)
 		isVisit[i] = new bool[_m.cols];
@@ -272,28 +155,16 @@ int main()
 	Mat resMat = 255 * Mat::ones(_m.rows, _m.cols, CV_8UC1);
 	int n_cnt = 0;
 	int rx = rand() % _m.rows, ry = rand() % _m.cols;
-	mPoint next(rx,ry,0);
-	while (cnt < pVec.size() * 95 / 100)
+	mPoint next(rx, ry, 0);
+	while (cnt < pVec.size() * 999 / 1000)
 	{
-		// cout << cnt << endl;
-		// int rx = rand() % _m.rows, ry = rand() % _m.cols;
-		
-		// cout << rx << ", " << ry << ", " << int(_m.at<uchar>(rx, ry)) << endl;
 		mPoint end = go(next.x, next.y, resMat, NUM, 0);
-		// next = bfs_next(end);
-		next = ch_next(end);
-		// printf("%d %d\n", next.x, next.y);
-		if (next.x==-1 && next.y==-1)
-		{	
-			int sum_bfs = 0;
-			for (int i = 0; i < _m.rows; ++i)
-				for (int j = 0; j < _m.cols; ++j)
-					sum_bfs += bfs_flag[i][j];
-			printf("%d\n", sum_bfs);
+		next = getNextPoint(end);
+		if (next.x == -1 && next.y == -1)
 			break;
-		}
-		// cout<<d<<endl;
-		if (end.gray>60)
+		
+		int fooDeep = end.gray;
+		if (fooDeep > min(_m.cols / 8, _m.rows / 8))
 		{
 			n_cnt ++ ;
 			stringstream ss;
@@ -306,13 +177,11 @@ int main()
 
 	generateVideo(n_cnt);
 
+
+	// free memory
 	for (int i = 0; i < _m.rows; ++i)
 		delete[] isVisit[i];
 	delete[] isVisit;
-
-	for (int i = 0; i < _m.rows; ++i)
-		delete[] bfs_flag[i];
-	delete[] bfs_flag;
 
 	return 0;
 }
